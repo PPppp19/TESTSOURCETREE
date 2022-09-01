b77d19217cb462256e0266023667e5a936659ed7
PPHORBUN <phongsathon.pho@gmail.com>
2022-09-01 10:04:36 +0700

    added some text

---
+++ TESTSOURCETREE/src/main/java/com/mycompany/testsourcetree/TESTSOURCETREE.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testsourcetree;
+
+/**
+ *
+ * @author Phongsathon
+ */
+public class TESTSOURCETREE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+      
+        String add =  "Added";  
+         System.out.println(add);
+    }
+    
+    
+    public String  add(){
+        
+        String a = "ADDED"; 
+        
+        return a; 
+        
+        
+    }
+}
